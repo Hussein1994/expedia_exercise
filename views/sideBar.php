1d5abedb9e1e5bb7ac5d6e7a71c1c34e72cbe5f7
Laith Ekermawi <laith.ekrmawi@gmail.com>
2017-02-20 02:18:59 +0200

    use bootstrap on side bar

--- views/sideBar.php
+++ views/sideBar.php
@@ -1,13 +1,13 @@
 <div class="left">
     <form id="form">
         <label for="minTripStartDate">Date start's before:</label><br>
-        <input name="minTripStartDate" type="text" class="datepicker" value="<?php echo $filters['minTripStartDate']; ?>"><br>
+        <input name="minTripStartDate" type="text" class="datepicker form-control" value="<?php echo $filters['minTripStartDate']; ?>"><br>
         <label for="maxTripStartDate">Date start's after:</label><br>
-        <input name="maxTripStartDate" type="text" class="datepicker" value="<?php echo $filters['maxTripStartDate']; ?>"><br>
+        <input name="maxTripStartDate" type="text" class="datepicker form-control" value="<?php echo $filters['maxTripStartDate']; ?>"><br>
         <label for="lengthOfStay">Length of stay:</label><br>
-        <input id="lengthOfStay" name="lengthOfStay" type="number" min="1" value="<?php echo $filters['lengthOfStay']; ?>"><br>
+        <input id="lengthOfStay" name="lengthOfStay" class="form-control" type="number" min="1" value="<?php echo $filters['lengthOfStay']; ?>"><br>
         <label for="destinationCity">Destination city:</label><br>
-        <input name="destinationCity" type="text" value="<?php echo $filters['destinationCity']; ?>"><br>
+        <input name="destinationCity" type="text" class="form-control" value="<?php echo $filters['destinationCity']; ?>"><br>
 
         <label>Min rate:</label><br>
         <input type="radio" name="minStarRating" value="1" <?php echo ($filters['minStarRating'] == 1) ? "checked" : ""; ?>>
@@ -32,7 +32,7 @@
         <label for="maxStarRating"><span>★</span><span>★</span><span>★</span><span>★</span></label><br>
         <input type="radio" name="maxStarRating" value="5" <?php echo ($filters['maxStarRating'] == 5) ? "checked" : ""; ?>>
         <label for="maxStarRating"><span>★</span><span>★</span><span>★</span><span>★</span><span>★</span></label><br>
-        <input type="submit" value="Filter">
-        <input type="button" id="reset" value="Reset">
+        <input type="submit" value="Filter" class="btn btn-success">
+        <input type="button" id="reset" value="Reset" class="btn btn-danger">
     </form>
 </div>
